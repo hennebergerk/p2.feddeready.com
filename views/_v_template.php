ef8ceb28189b90fd1bf83ad2a802a32581e9cd3b
Kevin Henneberger <henneberger@g.harvard.edu>
2013-11-03 11:34:52 -0500

    preliminary CSS and homepage updates

--- views/_v_template.php
+++ views/_v_template.php
@@ -3,7 +3,11 @@
 <head>
 	<title><?php if(isset($title)) echo $title; ?></title>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+	<!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">	
 					
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
@@ -36,5 +40,23 @@
 
     <?php if(isset($content)) echo $content; ?>
 
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,1 +1,8 @@
 <h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
+
+<p><a href="/posts/add">Add a post</a><br>
+
+<a href="/posts/users">Browse/follow users</a><br>
+
+<a href="/posts">Posts by users you follow</a> 
+</p>

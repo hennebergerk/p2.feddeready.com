fe3727a4ea099f20bd500c050c9cbad310ea786e
Kevin Henneberger <henneberger@g.harvard.edu>
2013-11-03 16:11:08 -0500

    logged in vs logged out homepage links

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,12 +1,20 @@
 
 
 <h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
+   
+        <?php if($user): ?>
 
-<p><a href="/posts/add">Add a post</a><br>
+			<p>
+			<a href="/posts/add">Add a post</a><br>
+			<a href="/posts/users">Browse/follow users</a><br>
+			<a href="/posts">Posts by users you follow</a> 
+			</p>
 
-<a href="/posts/users">Browse/follow users</a><br>
+        <?php else: ?>
 
-<a href="/posts">Posts by users you follow</a> 
+        	<p>
+            <a href='/users/signup'>Sign up</a>
+            <a href='/users/login'>Log in</a>
+            </p>
 
-</p>
-
+        <?php endif; ?>

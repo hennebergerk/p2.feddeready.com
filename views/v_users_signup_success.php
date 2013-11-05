9bb1d6651ece63f58935af9497b4b07e0afb0454
Kevin Henneberger <henneberger@g.harvard.edu>
2013-11-04 21:12:51 -0500

    2nd live test - confirmation email

--- views/v_users_signup_success.php
+++ views/v_users_signup_success.php
@@ -1,3 +1,5 @@
+
+
 <META HTTP-EQUIV="refresh" CONTENT="3;URL=/users/login">
 
 <h1>Congratulations!</h1>
@@ -7,13 +9,15 @@
 <p>If you are not redirected to the login page in 5 seconds, <a href="/users/login">click here</a></p>
 
 <?php
-$to[]    = Array("name" => APP_NAME, "email" => SYSTEM_EMAIL);
-$from    = Array("name" => APP_NAME, "email" => APP_EMAIL);
-$subject = "Welcome to Ready Set Blog";             
-    
-$body = "Congrats! You have completed registration to Ready Set Blog."
-    
-# Send email
-Email::send($to, $from, $subject, $body, true, '');
+class Email {
+	$to[]    = Array("name" => APP_NAME, "email" => SYSTEM_EMAIL);
+	$from    = Array("name" => APP_NAME, "email" => APP_EMAIL);
+	$subject = "Welcome to Ready Set Blog";				
+	
+	$body = "Congrats! You have completed registration to Ready Set Blog."
+	
+	# Send email
+	Email::send($to, $from, $subject, $body, true, '');
+}
 
 ?>

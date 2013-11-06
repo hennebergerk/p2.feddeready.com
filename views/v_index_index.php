49f0bb039c02927a7c71931f3484dc2950af88d8
Kevin Henneberger <henneberger@g.harvard.edu>
2013-11-05 22:20:40 -0500

    code cleanup

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,27 +1,27 @@
-        <div class="container">
-        	<div class="page-header">
-				<h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
- 			</div>
+<div class="container">
+    <div class="page-header">
+		<h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
+ 	</div>
 
-        	<?php if($user): ?>
+    <?php if($user): ?>
 
-            <p>
-            <a href="/posts/add">Add a post</a><br>
-            <a href="/posts/users">Browse/follow users</a><br>
-            <a href="/posts">Posts by users you follow</a> 
-            </p>
+    <p>
+    <a href="/posts/add">Add a post</a><br>
+    <a href="/posts/users">Browse/follow users</a><br>
+    <a href="/posts">Posts by users you follow</a> 
+    </p>
 
-        <?php else: ?>
+<?php else: ?>
 
-            <p>
-            <a href='/users/signup'>Sign up</a><br>
-            <a href='/users/login'>Log in</a>
-            <p>+1 features:
-            	<ul>
-					<li>Delete a post</li>
-					<li>Edit a post</li>
-				</ul>
-            </p>
+    <p>
+    <a href='/users/signup'>Sign up</a><br>
+    <a href='/users/login'>Log in</a>
+    <p>+1 features:
+        <ul>
+			<li>Delete a post</li>
+			<li>Edit a post</li>
+		</ul>
+    </p>
 
-        <?php endif; ?>
-        </div>
+<?php endif; ?>
+</div>
